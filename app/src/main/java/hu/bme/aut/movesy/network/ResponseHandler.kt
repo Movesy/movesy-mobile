3c3a5011b94a9a5e02917d958ed48b8fdb46256d
Tibi3k <Solyomtibi2000@gmail.com>
2021-10-15 00:33:01 +0200

    updated response handler

--- app/src/main/java/hu/bme/aut/movesy/network/ResponseHandler.kt
+++ app/src/main/java/hu/bme/aut/movesy/network/ResponseHandler.kt
@@ -1,23 +1,29 @@
 package hu.bme.aut.movesy.network
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 
-class ResponseHandler<T> (val lambda: () -> Unit): Callback<T> {
+class ResponseHandler<T> (
+    private val context: Context,
+    private val onSuccess: () -> Unit,
+
+): Callback<T> {
 
     override fun onResponse(call: Call<T>, response: Response<T>) {
         if(response.isSuccessful){
             Log.d("network", "Request successful")
-            lambda()
+            onSuccess()
         } else {
-            Log.w("network", "request failed: ${response.message()}")
+            Toast.makeText(context, response.message().toString(), Toast.LENGTH_LONG).show()
         }
 
     }
 
     override fun onFailure(call: Call<T>, t: Throwable) {
-        Log.w("network", "request failed: ${t.cause}")
+        Toast.makeText(context, t.cause.toString(), Toast.LENGTH_LONG).show()
     }
 }
